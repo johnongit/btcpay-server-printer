645f0695542ae20bb43c293d2d803ec07203d3d4
evildido <fraga_jonathan@protonmail.ch>
2022-12-07 22:56:17 +0100

    Cleanup dependencies
    Use template rendering in order to prefill config form

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ import machine
 import time
 import network
 import uasyncio
-from microdot_asyncio import Microdot, send_file
+from microdot_asyncio import Microdot, send_file, Response
+from microdot_utemplate import render_template
 import hashlib
 import hmac
-import urequest
 import json
-import sys
 import ntptime
-import os
 import micropython
 micropython.mem_info(1)
-from microWebCli import MicroWebCli
 
 
 # Initialize Microdot server
@@ -42,10 +39,6 @@ except:
         "wifi_psk": ""
     }
 
-wlan_sta = network.WLAN(network.STA_IF)
-ssid = params['wifi_ssid']
-password = params['wifi_psk']
-
 
 # Btcpay server url
 BTCPAY_INSTANCE=params['btcpay_server_url']
@@ -65,24 +58,13 @@ LONG_PRESS_DURATION = 500
 # Global variable that store button press duration
 button_press_time = None
 # Configure button
-boot_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+boot_button1 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 # Global ap_mode variable
 ap_mode = False
 
-# if button is pressed during more than 5 seconds, then start wifi in AP mode
-def start_ap_mode():
-  global ap_mode
-  ap_mode = True
-  ifconfig = create_AP()
-  # give some visual information
-  uasyncio.run(main_blink())
-  print("AP mode : " + str(ifconfig))
-  
+ 
 ############################################
   
-boot_button1 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
-
-
 button_down = False
 button_down_time = 0
 config_mode = False
@@ -117,25 +99,31 @@ def handle_button_rise(change):
   
 ################################################
 
-
-
-
-
 # Used to render date time for printer
 def date():
     # Get the current time including the timezone offset
     timestamp = time.localtime(time.time() + UTC_OFFSET)
-    # Format the hour, minute, and second as two-digit strings
-    hour = str(timestamp[3]).zfill(2)
-    minute = str(timestamp[4]).zfill(2)
-    second = str(timestamp[5]).zfill(2)
-    # Return the formatted date and time string
-    return hour + ':' + minute + ':' + second
+    if timestamp[3] < 10:
+        hour = '0' + str(timestamp[3])
+    else:
+        hour = str(timestamp[3])
+    if timestamp[4] < 10:
+        minute = '0' + str(timestamp[4])
+    else:
+        minute = str(timestamp[4])
+    if timestamp[5] < 10:
+        second = '0' + str(timestamp[5])
+    else:
+        second = str(timestamp[5])
+    date = hour +':' + minute +':' + second
+    print(date)
+    return str(date)
 
 
 # Get Invoice from btcpay server api
 def get_invoice(storeId, invoiceId):
     # API call that gets order from btcpay server
+    from microWebCli import MicroWebCli
 
     # Retry up to 5 times if the API call fails
     for retries in range(5):
@@ -194,54 +182,66 @@ def get_invoice(storeId, invoiceId):
 
 ##########################
     
+# Return config page
 @app.route('/',methods=['GET'])
-async def index(request):
-    return send_file('/config.html')
+async def index1(request):
+    global params
+    if config_mode:
+        Response.default_content_type = 'text/html'
+        return render_template('config.html', params=params)
+    return ""
+
 
 @app.route('/config',methods=['POST'])
 async def index(request):
-    wifi_ssid = request.form.get('wifi_ssid')
-    wifi_password = request.form.get('wifi_password')
-    url = request.form.get('url')
-    api_key = request.form.get('api_key')
-    webhook_secret = request.form.get('webhook_secret')
-    config = {
-      "wifi_ssid": wifi_ssid,
-      "wifi_psk": wifi_password,
-      "btcpay_server_url": url,
-      "btcpay_server_api_key": api_key,
-      "btcpay_server_webhook_secret": webhook_secret      
-    }
-    with open('params.json', 'w') as outfile:
-        json.dump(config, outfile)
-    print(config)
-    return "OK"
+    if config_mode:
+        wifi_ssid = request.form.get('wifi_ssid')
+        wifi_password = request.form.get('wifi_password')
+        url = request.form.get('url')
+        api_key = request.form.get('api_key')
+        webhook_secret = request.form.get('webhook_secret')
+        config = {
+          "wifi_ssid": wifi_ssid,
+          "wifi_psk": wifi_password,
+          "btcpay_server_url": url,
+          "btcpay_server_api_key": api_key,
+          "btcpay_server_webhook_secret": webhook_secret      
+        }
+        with open('params.json', 'w') as outfile:
+            json.dump(config, outfile)
+        print(config)
+        return "OK"
+    else:
+        return ""
 
 # Create route that accept POST method
 @app.route('/',methods=['POST'])
 def index(request):
-    headers = request.headers
-    post_data_raw = request.body
-    post_data_json = request.json
-    print(headers)
-    print(post_data_raw)
-    try:
-        # Check webhook signature sent by btcpay server
-        sig = headers['btcpay-sig'][7:] 
-        print("sig pushed", sig)
-        sig_computed = hmac.new(bytes(secret, "utf-8"), msg=post_data_raw, digestmod=hashlib.sha256).hexdigest()
-        print("sig computed", sig_computed)
-        if sig_computed != sig:
-            return 'Unauthorized', 401
-        if(post_data_json["type"] == "InvoiceSettled"):
-            print("InvoiceSettled")
-            invoiceId = post_data_json['invoiceId']
-            storeId = post_data_json['storeId']
-            get_invoice(storeId, invoiceId)
-        return 'OK'
-    except Exception as e:
-        print("Error: ", e)
-        return "Canno Parse request"
+    if not config_mode:
+        headers = request.headers
+        post_data_raw = request.body
+        post_data_json = request.json
+        print(headers)
+        print(post_data_raw)
+        try:
+            # Check webhook signature sent by btcpay server
+            sig = headers['btcpay-sig'][7:] 
+            print("sig pushed", sig)
+            sig_computed = hmac.new(bytes(secret, "utf-8"), msg=post_data_raw, digestmod=hashlib.sha256).hexdigest()
+            print("sig computed", sig_computed)
+            if sig_computed != sig:
+                return 'Unauthorized', 401
+            if(post_data_json["type"] == "InvoiceSettled"):
+                print("InvoiceSettled")
+                invoiceId = post_data_json['invoiceId']
+                storeId = post_data_json['storeId']
+                get_invoice(storeId, invoiceId)
+            return 'OK'
+        except Exception as e:
+            print("Error: ", e)
+            return "Canno Parse request"
+    else:
+        return ""
     
 ##########################
 
@@ -256,7 +256,7 @@ def do_connect():
     wlan_sta.active(True)
     if wlan_sta.isconnected():
         led.value(1)
-        return None
+        return wlan_sta.ifconfig()
     
     # Print message and attempt to connect to WiFi network
     print('Trying to connect to %s...' % ssid)
@@ -285,8 +285,8 @@ def do_connect():
             led.value(1)
             time.sleep(0.2)
             led.value(0)
-            time.sleep(0.2)       
-    return connected
+            time.sleep(0.2)
+    return wlan_sta.ifconfig()
 
 
 # Used to configure esp as an access point (used for configuration mode)
@@ -296,7 +296,7 @@ def create_AP():
     ap_if.active(True)
 
     # Set up access point with specified parameters
-    ap_if.config(essid='micropython', password=b"micropython", channel=11, authmode=network.AUTH_WPA_WPA2_PSK)
+    ap_if.config(essid='micropython', password=b"dkzaopdeazefhu", channel=11, authmode=network.AUTH_WPA_WPA2_PSK)
     
     # Return access point configuration
     return ap_if.ifconfig()
@@ -313,19 +313,20 @@ async def main():
     print("start")
     
     led.value(0)
-    
+ 
     try:
         # Start Wifi
-        do_connect()
-        ifconfig = wlan_sta.ifconfig()
+        ifconfig = do_connect()
         uasyncio.create_task(start_server())               
         #Print info in debug console and thermal printer
-        printer.println('Wifi Connected \r\n IP Address: ' + ifconfig[0])
+        print('Wifi Connected \r\n IP Address: ' + str(ifconfig[0]))
+        printer.println('Wifi Connected \r\n IP Address: ' + str(ifconfig[0]))
         printer.println("Server Started")
         printer.feed(2)
         ntptime.settime()
         printer.println(date())
-    except:
+    except Exception as e:
+        print(e)
         print("config file not exist")
         uasyncio.create_task(start_server())
         config_mode = True
@@ -360,4 +361,4 @@ uasyncio.run(main())
 
 
 
-
+z
